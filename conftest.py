5ec6b45f64a7fe2c27bd638a51db7174de385eaa
Andrey <adenisov.pro@gmail.com>
2022-10-15 10:15:19 +0300

    Create new files

--- conftest.py
+++ conftest.py
@@ -1,1 +1,49 @@
-...
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name',
+                     action='store',
+                     default='chrome',
+                     help="Choose browser: chrome"
+                     )
+    parser.addoption('--language',
+                     action='store',
+                     default=None,
+                     help="Choose from langs: en/ru/es/...)"
+                     )
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+
+
+
+
+
+
+# @pytest.fixture(scope="function")
+# def browser():
+#     print("\nstart browser for test..")
+#     browser = webdriver.Chrome()
+#     yield browser
+#     print("\nquit browser..")
+#     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,1 +1,27 @@
-...
+# Путь для запуска:
+# pytest --language=es test_items.py
+
+
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+
+
+def test_launching_autotests_for_different_languages(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(30)
+
+    wait = WebDriverWait(browser, 5)
+    button_text = wait.until(
+        ec.visibility_of_element_located((
+            By.CSS_SELECTOR,
+            "button.btn.btn-lg.btn-primary.btn-add-to-basket"
+        ))
+    ).text
+
+    assert button_text is not None, "Страница товара на сайте не содержит кнопку добавления в корзину"
+
